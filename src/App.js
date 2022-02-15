074ed2141873a7a83e96ac368e68c8e839fa481f
garlife <garlife@gmail.com>
2022-02-16 01:04:25 +0300

    + other conditions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import React, { useState } from 'react';
 import './style.css';
-import Conditions from './Conditions';
+import TernJsxConditions, {
+  IfElseConditions,
+  SwitchConditions,
+  DoubleAmpersand
+} from './Conditions';
 
 export default function App() {
-  
+  const [selected, setSelected] = useState('none');
+  function handleChange(event) {
+    setSelected(event.target.value);
+    console.log(selected);
+    return selected;
+  }
+
   return (
     <div className="container">
-      <Conditions />
+      <div className={selected} style={{width: "25%"}}>
+        <h1>Выберите время года</h1>
+        <p>
+          <select value={selected} onChange={handleChange}>
+            <option value="none">Не выбрано</option>
+            <option value="winter">Зима</option>
+            <option value="spring">Весна</option>
+            <option value="summer">Лето</option>
+            <option value="autumn">Осень</option>
+          </select>
+        </p>
+        <TernJsxConditions selected={selected} />
+      </div>
+      <div style={{width: "25%"}}>
+        <IfElseConditions selected={selected} />
+      </div>
+      <div style={{width: "25%"}}>
+        <SwitchConditions selected={selected} />
+      </div>
+      <div style={{width: "25%"}}>
+        <DoubleAmpersand selected={selected} />
+      </div>
     </div>
   );
 }
--- src/Conditions.js
+++ src/Conditions.js
@@ -1,39 +1,161 @@
 import React, { useState } from 'react';
 import './style.css';
 
-export default function Conditions() {
-  const [selected, setSelected] = useState('none');
-
-  function handleChange(event) {
-    setSelected(event.target.value);
-    console.log(selected);
-    return selected;
+export function IfElseConditions(props) {
+  if (props.selected === 'none') {
+    return (
+      <div className={'none'} style={{ height: '1000px' }}>
+        <div className="season">
+          <p></p>
+        </div>
+      </div>
+    );
+  } else if (props.selected === 'winter') {
+    return (
+      <div className={'winter'} style={{ height: '1000px' }}>
+        <div className="season">
+          <p>Зима</p>
+        </div>
+      </div>
+    );
+  } else if (props.selected === 'spring') {
+    return (
+      <div className={'spring'} style={{ height: '1000px' }}>
+        <div className="season">
+          <p>Весна</p>
+        </div>
+      </div>
+    );
+  } else if (props.selected === 'summer') {
+    return (
+      <div className={'summer'} style={{ height: '1000px' }}>
+        <div className="season">
+          <p>Лето</p>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className={'autumn'} style={{ height: '1000px' }}>
+        <div className="season">
+          <p>Осень</p>
+        </div>
+      </div>
+    );
   }
 
-  return (
-    <div className="container">
-    <div className={selected}
-    
-    >
-      <h1>Выберите время года</h1>
-      <p>
-        <select value={selected} onChange={handleChange}>
-          <option value="none">Не выбрано</option>
-          <option value="winter">Зима</option>
-          <option value="spring">Весна</option>
-          <option value="summer">Лето</option>
-          <option value="autumn">Осень</option>
-        </select>
-        </p>
-        <div className="season">
-          { 
-          selected === "none" ? "" :
-          selected === "winter" ? "Зима" :
-          selected === "spring" ? "Весна" :
-          selected === "summer" ? "Лето" : "Осень"
-         }
+  return null;
+}
+
+export function SwitchConditions(props) {
+  switch (props.selected) {
+    case 'none':
+      return (
+        <div className={'none'} style={{ height: '1000px' }}>
+          <div className="season">
+            <p></p>
+          </div>
         </div>
-    </div>
-    </div>
+      );
+
+    case 'winter':
+      return (
+        <div className={'winter'} style={{ height: '1000px' }}>
+          <div className="season">
+            <p>Зима</p>
+          </div>
+        </div>
+      );
+
+    case 'spring':
+      return (
+        <div className={'spring'} style={{ height: '1000px' }}>
+          <div className="season">
+            <p>Весна</p>
+          </div>
+        </div>
+      );
+
+    case 'summer':
+      return (
+        <div className={'summer'} style={{ height: '1000px' }}>
+          <div className="season">
+            <p>Лето</p>
+          </div>
+        </div>
+      );
+
+    case 'autumn':
+      return (
+        <div className={'autumn'} style={{ height: '1000px' }}>
+          <div className="season">
+            <p>Осень</p>
+          </div>
+        </div>
+      );
+  }
+}
+
+export function DoubleAmpersand(props) {
+  return (
+    <>
+      {props.selected === 'none' &&
+        <div className={'none'} style={{ height: '1000px' }}>
+          <div className="season">
+            <p></p>
+          </div>
+        </div>
+      }
+
+      {props.selected === 'winter' &&
+        <div className={'winter'} style={{ height: '1000px' }}>
+          <div className="season">
+            <p>Зима</p>
+          </div>
+        </div>
+      }
+      {props.selected === 'spring' &&
+        <div className={'spring'} style={{ height: '1000px' }}>
+          <div className="season">
+            <p>Весна</p>
+          </div>
+        </div>
+      }
+
+      {props.selected === 'summer' &&
+        <div className={'summer'} style={{ height: '1000px' }}>
+          <div className="season">
+            <p>Лето</p>
+          </div>
+        </div>
+      }
+      {props.selected === 'autumn' &&
+        <div className={'autumn'} style={{ height: '1000px' }}>
+          <div className="season">
+            <p>Осень</p>
+          </div>
+        </div>
+      }
+    </>
   );
 }
+
+function TernJsxConditions(props) {
+  return (
+    <>
+      <div className="season">
+        {props.selected === 'none'
+          ? ''
+          : props.selected === 'winter'
+          ? 'Зима'
+          : props.selected === 'spring'
+          ? 'Весна'
+          : props.selected === 'summer'
+          ? 'Лето'
+          : 'Осень'}
+      </div>
+    </>
+  );
+}
+
+export default TernJsxConditions;
